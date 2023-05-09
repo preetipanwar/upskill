3009155ae0da3c8aa058daa3f136311d3341e242
preetipanwar <preetipanwar102@gmail.com>
2023-05-09 15:11:40 +0530

    prevent default e

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,11 +1,13 @@
 import Gallery from './components/Gallery'
 import Card from './components/Card';
 import Cards from './components/Cards';
+import RespondingToEvents from './components/Adding-Interactivity/RespondingToEvents';
 
 export default function App() {
   return (
     <>
       <div className="container">
+       <RespondingToEvents buttonName = "do it" />
         <Gallery />
         <Cards />
         <Card />
