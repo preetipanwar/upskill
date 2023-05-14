9445daad230fe27a1f00b0b1f90b012c9709f44a
preetipanwar <preetipanwar102@gmail.com>
2023-05-14 16:37:21 +0530

    change folder structure

---
+++ react/src/components/react-dev/tasks/Todo.jsx
@@ -0,0 +1,24 @@
+import todolist from '../../../data/todoList';
+
+export default function TodoList() {
+  return (
+    <>
+      <Todo name={todolist[0].name} items={todolist[0].items} />
+      <Todo name={todolist[1].name} items={todolist[1].items} />
+      <Todo name={todolist[2].name} items={todolist[2].items} />
+    </>
+  );
+}
+
+export function Todo({ name, items }) {
+  return (
+    <div className="card">
+      <h2>{name}</h2>
+      <ul>
+        {items.map(function (item) {
+          return <li>{item}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
