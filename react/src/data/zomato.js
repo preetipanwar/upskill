f37a0750371ac76fa051af13bb524dff30851fa7
preetipanwar <preetipanwar102@gmail.com>
2023-05-17 23:03:26 +0530

    added new data file

---
+++ react/src/data/zomato.js
@@ -0,0 +1,47 @@
+const cardData = [
+  {
+    url: 'https://b.zmtcdn.com/webFrontend/e5b8785c257af2a7f354f1addaf37e4e1647364814.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*',
+    tagLine: 'Order Online',
+    description: 'Stay home and order to your doorstep',
+  },
+  {
+    url: 'https://b.zmtcdn.com/webFrontend/d026b357feb0d63c997549f6398da8cc1647364915.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*',
+    tagLine: 'Dining',
+    description: "View the city's favourite dining venues",
+  },
+  {
+    url: 'https://b.zmtcdn.com/webFrontend/d9d80ef91cb552e3fdfadb3d4f4379761647365057.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*',
+    tagLine: 'Nightlife and Clubs',
+    description: "Explore the city's top nighlife outlets",
+  },
+];
+
+const collections = [
+  {
+    heading: '18 Thrilling Live Cricket Screenings',
+    subHeading:
+      'Treat yourself to an irresistible pairing of pulse-racing cricket action and lip-smacking delicacies at top places in the city!',
+    places: '18 places',
+  },
+  {
+    heading: '22 Great Cafes',
+    subHeading:
+      'Pamper yourself with a French croissant or add an Italian flair to the sandwich! Here’s our handpicked list of awesome cafes in town.',
+    places: '23 places',
+  },
+  {
+    heading: '18 Newly Opened Restaurants',
+    subHeading:
+      'Chic newcomers bringing a gush of newness and aroma of deliciousness. Updated every Thursday!',
+    places: '12 places',
+  },
+  {
+    heading: '9 Unique Dining Experiences',
+    subHeading:
+      'From relishing meals in a luxury train coach to sipping cocktails in a revolving restaurant, these places promise indelible experiences.',
+    places: '9 places',
+  },
+];
+
+module.exports.cardData = cardData;
+module.exports.collections = collections;
