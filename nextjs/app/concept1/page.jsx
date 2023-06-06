7e67f3dfa5c6d8af8fc2211d6407bc7a64b2feec
preetipanwar <preetipanwar102@gmail.com>
2023-06-06 18:07:31 +0530

    vegetables

--- nextjs/app/concept1/page.jsx
+++ nextjs/app/concept1/page.jsx
@@ -5,15 +5,27 @@ export default function page() {
   return (
     <div>
       {vegetables.map(function (item) {
+        const color = item.stocked ? 'green' : 'pink';
         return (
-          <div className="p-3 m-3 bg-gray-500 w-80">
-            <p>Name: {item.name}</p>
-            <p>Category: {item.category}</p>
-            <p>Price: {item.price}</p>
-            <p>Instock: {item.stocked}</p>
+          <div
+            style={{ backgroundColor: color }}
+            className="p-3 m-3 bg-gray-500 w-60 rounded-md mx-auto"
+          >
+            <div className="flex justify-between space-y-2">
+              <p className="text-black">{item.name}</p>
+              <p>{item.stocked ? '✔' : 'x'}</p>
+            </div>
+            <div className="flex space-x-1">
+              <p className="py-1 px-2 bg-orange-800 w-fit rounded">
+                {item.category}
+              </p>
+              <p className="py-1 px-2 bg-black w-fit rounded"> {item.price}</p>
+            </div>
           </div>
         );
       })}
     </div>
   );
 }
+
+// condition ? 'true statement' : 'false statement';
